194f5361d0e0e3b1e44737bbfcecda1cc7fb7e58
HongKorea <edujoo24@gmail.com>
2023-04-27 06:48:28 -0300

    My commit comments

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,14 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true
+})
+
 const path = require('path')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/my-project/'
+    ? '/vuecinema/'
     : '/',
-  outputDir: path.resolve(__dirname, 'dist'),
-  // ...
+  outputDir: path.resolve(__dirname, './dist'),
+  assetsDir: 'static'
 }
-
